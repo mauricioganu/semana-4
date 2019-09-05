8e7d4c756aa121cea87a08788adea42f515c396a
mauricioganu <54649732+mauricioganu@users.noreply.github.com>
2019-09-04 21:29:17 -0300

    Add files via upload

--- action_page.php
+++ action_page.php
@@ -4,9 +4,18 @@
 //var_dump($_GET);
 echo "su nombre es " .$_GET['nombre'];
 
+//$archivo=fopen('usuarios.txt', 'a');
+
+$MiObjeto= new stdClass();
+$MiObjeto->nombre=$_GET['nombre'];
+$MiObjeto->apellido=$_GET['apellido'];
+
+var_dump($_GET);
+var_dump(json_encode($MiObjeto));
 $archivo=fopen('usuarios.txt', 'a');
 fwrite($archivo, $_GET['nombre']."\n");
-
-
+var_dump(json_encode($MiObjeto));
+fclose($archivo);
+ 
 
 ?>
